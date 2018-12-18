d816f5c9aa462e694a2780e4a185c7051ba96e5f
chichi77 <scps940327@gmail.com>
2018-12-18 09:42:11 +0800

    建置基本專案設定
    1. 安裝Bootstrap
    2. Webpack的CSS分檔設定
    3. 櫻花學舊header.scss、footer.scss檔案整合

---
+++ src/index.js
@@ -0,0 +1,4 @@
+//import FormContainer from "./js/components/container/FormContainer";
+import App from "./js/components/container/DateContainer";
+
+require('./scss/style.scss');
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+      main:"./src/index.js",
+      //about:"./src/about.js"
+  },
+  output: {
+      path: __dirname + '/dist',
+      filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  // plugins: [
+  //   new HtmlWebPackPlugin({
+  //     template: "./src/index.html",
+  //     filename: "./index.html"
+  //   })
+  // ]
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      chunks: ['main'] // 預設會將打包出的所有 js 插入 html。故需指明頁面需要的模組
+    }),
+    // new HtmlWebPackPlugin({
+    //   template: 'src/index.html',
+    //   filename: 'about.html',
+    //   chunks: ['about']
+    // }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "./css/style.css"
+    })
+  ]
+};
